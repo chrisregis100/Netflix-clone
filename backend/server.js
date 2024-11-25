0b8dff885c20d68bf20d35fa4adfd4492304360c
Chris2400 <erneregis@gmail.com>
2024-11-25 14:27:48 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,10 @@ app.use(
   );
 app.use("/api", UserRouter);
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(port, ()=>{
  console.log(`le server  a demarré au port ${port}`})
 )
